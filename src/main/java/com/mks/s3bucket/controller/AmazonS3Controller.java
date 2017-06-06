bab0b2d170635fb9dc0a022987108bee8bdfed3c
mks3251 <mksloan6@gmail.com>
2017-06-06 18:10:42 -0500

    add query param to url

--- src/main/java/com/mks/s3bucket/controller/AmazonS3Controller.java
+++ src/main/java/com/mks/s3bucket/controller/AmazonS3Controller.java
@@ -1,12 +1,18 @@
 package com.mks.s3bucket.controller;
 
 
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.mks.s3bucket.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -17,8 +23,14 @@ public class AmazonS3Controller {
     @Autowired
     S3Service s3Service;
 
-    @RequestMapping("/api")
-    public S3Object getBucket() {
+    @RequestMapping(value="/api")
+    public List<S3ObjectSummary> getObjectInBucket() {
+        return s3Service.getObjectInBucket();
+    }
+
+    @RequestMapping(value= "/moodledeployables", method = RequestMethod.GET)
+    public Date getBucket(@RequestParam String modified) {
+        modified += "LATEST";
         return s3Service.getBucket();
     }
 
--- src/main/java/com/mks/s3bucket/service/S3Service.java
+++ src/main/java/com/mks/s3bucket/service/S3Service.java
@@ -1,13 +1,19 @@
 package com.mks.s3bucket.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by MK on 6/3/2017.
  */
 public interface S3Service {
-    S3Object getBucket();
+    Date getBucket();
+    List<S3ObjectSummary> getObjectInBucket();
 
 
 }
