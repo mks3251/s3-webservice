ce0757d5856d92248cdc50f4182f6de3c2351002
mks3251 <mksloan6@gmail.com>
2017-06-06 21:30:55 -0500

    Update AmazonS3Controller.java

--- src/main/java/com/mks/s3bucket/controller/AmazonS3Controller.java
+++ src/main/java/com/mks/s3bucket/controller/AmazonS3Controller.java
@@ -5,15 +5,22 @@ import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.mks.s3bucket.service.S3Service;
+import com.mks.s3bucket.service.impl.S3ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import sun.rmi.runtime.Log;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 /**
  * Created by MK on 6/2/2017.
@@ -28,10 +35,16 @@ public class AmazonS3Controller {
         return s3Service.getObjectInBucket();
     }
 
-    @RequestMapping(value= "/moodledeployables", method = RequestMethod.GET)
-    public Date getBucket(@RequestParam String modified) {
-        modified += "LATEST";
-        return s3Service.getBucket();
+    @RequestMapping(value="/moodledeployables", method = RequestMethod.GET)
+    public Date getObjectLastModified (@RequestParam String modified) {
+        if (modified.contentEquals("LATEST"))
+        {
+            return s3Service.getBucket();
+        }
+        else {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 
